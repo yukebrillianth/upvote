26e68e669aa9747482f0917b6235b60cf2b7812c
Yuke Brilliant H <61053094+yukebrillianth@users.noreply.github.com>
2023-06-28 20:16:58 +0700

    Revert "Database/migrations"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([SettingSeeder::class]);
-
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
